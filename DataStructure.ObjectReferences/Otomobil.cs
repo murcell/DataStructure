ee70ef8923be906f6c5e2fde9a7755b2fda81c74
murcell <murselgurkan@gmail.com>
2024-07-23 15:37:25 +0300

    Add project files.

---
+++ DataStructure.ObjectReferences/Otomobil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.ObjectReferences
+{
+    class Otomobil
+    {
+        public string Marka { get; set; }
+
+        public Otomobil(string marka)
+        {
+            this.Marka = marka;
+        }
+
+    }
+}
---
+++ DataStructure.ObjectReferences/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DataStructure.ObjectReferences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Otomobil otomobil1 = new Otomobil("Audi");
+            Console.WriteLine(otomobil1.Marka);
+
+            Otomobil otomobil2;
+            otomobil2 = otomobil1;
+
+            otomobil1 = new Otomobil("Opel");
+
+            Console.WriteLine(otomobil2.Marka);
+            Console.WriteLine(otomobil1.Marka);
+
+            otomobil2 = new Otomobil("Mercedes");
+
+            Console.WriteLine(otomobil2.Marka);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
