a22a83a66e73171fe363541854ed9a5f9b0a7862
murcell <murselgurkan@gmail.com>
2024-07-25 23:31:55 +0300

    DataStructure.Stacks was completed

---
+++ DataStructure.Stack/Data/Stack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Stacks.Data
+{
+    class Stack
+    {
+        private Node _top;
+
+        private int _count;
+        public Stack() 
+        {
+            _top = null;
+            _count = 0;
+        }
+
+        public Stack(int data) 
+        {
+            Node newNode = new Node(data);
+            _top = newNode;
+            _count = 1;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (_count == 0)
+            {
+                _top = newNode;
+            }
+            else
+            {
+                newNode.Next = _top;
+                _top = newNode;
+            }
+            _count++;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine("Stack içeriği : ");
+            Console.WriteLine("-------------------");
+            Node temp = _top;
+
+            while (temp != null)
+            {
+                Console.WriteLine($"{temp.Data}");
+                temp = temp.Next;
+            }
+        }
+
+        public void PrintTop()
+        {
+            Console.WriteLine($"Top : {_top.Data}" );
+        }
+
+        public void PrintCount()
+        {
+            Console.WriteLine($"Count : {_count}");
+        }
+
+        public Node Pop()
+        {
+            if (_count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Node removedNode = _top;
+
+                _top = _top.Next;
+                removedNode.Next = null;
+
+                _count--;
+                return removedNode;
+            }
+           
+        }
+
+        public bool IsEmpty()
+        {
+            return _count == 0; 
+        }   
+    
+    }
+}
---
+++ DataStructure.Stack/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using DataStructure.Stacks.Data;
+
+namespace DataStructure.Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack örnekleri");
+            Stack myStack = new Stack();
+            myStack.Push(6);
+            myStack.Push(14);
+            myStack.Push(32);
+            myStack.Push(21);
+            myStack.PrintAll();
+            Console.WriteLine("* * * * * *");
+
+            Node removed = myStack.Pop();
+            Console.WriteLine($"Silinen node : {removed.Data}");
+            myStack.PrintAll();
+            Console.WriteLine("* * * * * *");
+
+
+            //Console.WriteLine("------");
+            //myStack.PrintTop();
+            //myStack.PrintCount();
+            //Console.WriteLine("* * * * ");
+
+            //myStack.Push(50);
+            //myStack.PrintTop();
+            //myStack.PrintCount();
+            //myStack.PrintAll();
+        }
+    }
+}
