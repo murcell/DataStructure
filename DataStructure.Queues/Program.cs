5b669b606c2ee49850e5768b895620d7ca17c843
murcell <murselgurkan@gmail.com>
2024-07-25 23:38:47 +0300

    images were added

---
+++ DataStructure.Queues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure.Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
