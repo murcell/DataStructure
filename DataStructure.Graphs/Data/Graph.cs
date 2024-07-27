5346f2231a64bd697a11d4b8888cc24472090d88
murcell <murselgurkan@gmail.com>
2024-07-27 13:55:56 +0300

    Graphs examples were coded

---
+++ DataStructure.Graphs/Data/Graph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Graphs.Data
+{
+    class Graph
+    {
+        private Dictionary<int, List<int>> adjcList;
+
+        public Graph()
+        {
+            adjcList = new Dictionary<int, List<int>>();
+        }
+
+        public bool AddVertex (int vertex)
+        {
+            List<int> value;
+
+            if (adjcList.TryGetValue(vertex, out value))
+            {
+                // bir value mevcut
+                // bir grapha aynı verteksi bir daha eklemiyoruz
+                return false;
+            }
+            else 
+            {
+                // ekleme işlemi burada yapılacak
+                adjcList.Add(vertex, new List<int>());
+
+                return true;
+            }
+
+        }
+
+        public void PrintGraph()
+        {
+            foreach (var vertex in adjcList)
+            {
+                Console.Write($"{vertex.Key} : {{ ");
+                Console.Write(string.Join(", ", vertex.Value));
+                Console.WriteLine($" }}");
+
+            }
+        }
+
+        public bool AddEdge(int vertex1, int vertex2) 
+        { 
+            
+            if (adjcList.ContainsKey(vertex1) && adjcList.ContainsKey(vertex2))
+            {
+                adjcList[vertex1].Add(vertex2);
+                adjcList[vertex2].Add(vertex1);
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool RemoveEdge(int vertex1, int vertex2) 
+        {
+            if (adjcList.ContainsKey(vertex1) && adjcList.ContainsKey(vertex2))
+            {
+                adjcList[vertex1].Remove(vertex2);
+                adjcList[vertex2].Remove(vertex1);
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool RemoveVertex(int vertex) 
+        {
+            if (adjcList.ContainsKey(vertex)==false)
+                return false;
+           
+            // silinmesi isten vertexe edge'i olan diğer vertexlerin
+            // listeleri içinde silinecek vertexi bulup siliyoruz
+            foreach (var relatedVertex in adjcList[vertex]) 
+            { 
+                adjcList[relatedVertex].Remove(vertex);
+            }
+
+            // silinecek vertex key değerini ve value değerini kaldırıyoruz
+            adjcList.Remove(vertex);
+            return true;
+            //List<int> value;
+            //if (!adjcList.TryGetValue(vertex, out value))
+            //    return false;
+            //else
+            //{
+            //    foreach (var vertex2 in value) 
+            //    { 
+            //    }
+            //}
+
+        }
+
+    }
+}
---
+++ DataStructure.Graphs/Program.cs
@@ -0,0 +1,39 @@
+﻿using DataStructure.Graphs.Data;
+
+namespace DataStructure.Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Graphs örnekleri");
+
+
+            Graph graph = new Graph();
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+
+
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(2, 5);
+            graph.AddEdge(3, 5);
+            graph.AddEdge(4, 5);
+            graph.PrintGraph();
+            //Console.WriteLine("- - - - - - - - - - - - - - - - - -");
+            //graph.RemoveVertex(2);
+            //graph.PrintGraph();
+
+            //graph.RemoveEdge(1, 2);
+            //graph.RemoveEdge(1, 3);
+            //graph.PrintGraph();
+            //Console.WriteLine("***********************");
+            //graph.RemoveEdge(4,5);
+            //graph.PrintGraph();
+        }
+    }
+}
